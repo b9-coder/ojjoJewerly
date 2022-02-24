a8339ad8f4f75062cdaeea9a4f9c6356b4d92b8f
b9-coder <laytlsu@gmail.com>
2022-02-24 18:09:09 +0600

    '24'

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 $(
 	function() {
 		
-		$('.menu-burger__header').click(function() {
-			$('.menu-burger__header').toggleClass('open-menu')
-			$('.header__menu1').toggleClass('open-menu')
-		
+		$('.menu-burger__header').click(function(event) {
+			$('.menu-burger__header, .header__menu1').toggleClass('active');
+			
+
 		})
 	}
 );
 
 /*
+$(
+	function() {
+		
+		$('.menu-burger__header').click(function(event) {
+			$('.menu-burger__header, .header__menu1').toggleClass('active');
+			
+
+		})
+	}
+);
+
 			$('.menu').toggleClass('show-menu')
 			$('.header').css('background', 'rgba(0, 0, 0, 0.4)').css('height', '500px')
 			$('.menu__link').css('margin-top', '20px');
